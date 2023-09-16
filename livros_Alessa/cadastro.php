7d378517b9f9fd973309faed456fb43be388408d
meiraalessa <134645330+meiraalessa@users.noreply.github.com>
2023-09-16 11:45:07 -0300

    Add files via upload

---
+++ livros_Alessa/cadastro.php
@@ -0,0 +1,133 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="fonts/material-design-iconic-font/css/material-design-iconic-font.css">
+
+    <link href="https://db.onlinewebfonts.com/c/b41f182757f4cf61e19aae4dad595676?family=BF+Fiona+Script+W01+Bold" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=ADLaM+Display&family=Caveat:wght@500&family=Marcellus+SC&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+  
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    
+    <link rel="stylesheet" href="css/estilo_CadastroMenu.css">
+    <link rel="stylesheet" href="css/estilo_CadastroForm.css">
+    <link rel="stylesheet" href="css/estilo_CadastroFooter.css">
+
+  
+    <title>cadastre-se</title>
+  </head>
+  <body>
+
+<!-- cabeçalho -->
+
+<header class="o-header">
+    <div class="center">
+    <div class="logo"><img src="imagens/cafe.jpeg"/></div> <!--Logo-->
+    <div class="menu">
+      <a href="../PaginaPrincipal/index.php">Home</a>
+      <a href="../PaginaMenu/cardapio.html">Menu</a>
+                <a href="../livros.html">Livros</a>
+                <a href="../PaginaDoacoes/doacoes.php">Doações</a>
+                <a href="cadastro.php">Cadastro</a>
+    </div><!--menu-->
+    </div><!--center-->
+   
+  </header>
+    
+
+
+
+
+
+
+
+                  <!-- main -->
+    <main>
+    
+        
+    <div class="form-container">
+      
+        <form action="#" method="post">
+            <label for="login"><img src="imagens/user.png" alt="Login"></label>
+            <input type="text" id="login" name="login" required><br><br><br>
+
+            <label for="senha"><img src="imagens/senha.png" alt="Senha"></label>
+            <input type="password" id="senha" name="senha" required><br><br><br>
+
+            <label for="email"><img src="imagens/email.png" alt="Email"></label>
+            <input type="email" id="email" name="email" required><br><br><br>
+
+            <label for="telefone"><img src="imagens/contato.png" alt="Telefone"></label>
+            <input type="tel" id="telefone" name="telefone" required><br><br><br>
+
+            <div class="button-container">
+                <button type="button" id="new-user-button">New User</button>
+                <button type="submit" id="login-button">Login</button>
+            </div>
+        </form>
+    </div>
+ 
+    </main>
+
+    <!-- rodapé -->
+    <footer >
+    <div class="opening-hours">
+    <br>
+            <br>
+            <h3>OPENING HOURS</h3>
+            <ul>
+                <li><strong>Monday</strong> 6:00 – 23:00</li>
+                <br>
+            
+                <li><strong>Tuesday</strong> 6:00 – 23:00</li>
+                <br>
+           
+                <li><strong>Wednesday</strong> 6:00 – 23:00</li>
+                <br>
+            
+                <li><strong>Thursday</strong> 6:00 – 23:00</li>
+                <br>
+           
+                <li><strong>Friday</strong> 6:00 – 23:00</li>
+                <br>
+            
+                <li><strong>Saturday</strong> 3:00 – 23:00</li>
+                <br>
+          
+                <li><strong>Sunday</strong> 6:00 – 23:00</li>
+            </ul>
+        </div>
+        <div class="contacts">
+        <br>
+            <br>
+            <h3>CONTACTS</h3>
+            <br>
+            <p>@expresso_poesia</p>
+            <br>
+            <br>
+            
+            <p>expressopoesia@gmail.com</p>
+            <br>
+            <br>
+
+            <p>exepresso&poesia.com.br</p>
+            <br>
+            <br>
+            
+            <p>(+55) 77 98042-1803</p>
+            <br>
+            <br>
+            
+        </div>
+		
+		</footer>
+
+
+
+  </body>
+</html>
---
+++ livros_Alessa/doacoes.php
@@ -0,0 +1,131 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="fonts/material-design-iconic-font/css/material-design-iconic-font.css">
+
+    <link href="https://db.onlinewebfonts.com/c/b41f182757f4cf61e19aae4dad595676?family=BF+Fiona+Script+W01+Bold" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=ADLaM+Display&family=Caveat:wght@500&family=Marcellus+SC&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+  
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    
+    <link rel="stylesheet" href="css/estilo_Menu.css">
+    <link rel="stylesheet" href="css/estilo_DoacoesForm.css">
+    <link rel="stylesheet" href="css/estilo_Footer.css">
+
+  
+    <title>cadastre-se</title>
+  </head>
+  <body>
+
+<!-- cabeçalho -->
+
+<header class="o-header">
+    <div class="center">
+    <div class="logo"><img src="imagens/cafe.jpeg"/></div> <!--Logo-->
+    <div class="menu">
+      <a href="../PaginaPrincipal/index.php">Home</a>
+      <a href="../PaginaMenu/cardapio.html">Menu</a>
+                <a href="../livros.html">Livros</a>
+                <a href="doacoes.php">Doações</a>
+                <a href="../PaginaCadastro-/cadastro.php">Cadastro</a>
+    </div><!--menu-->
+    </div><!--center-->
+   
+  </header>
+    
+
+
+
+
+
+
+
+                  <!-- main -->
+    <main>
+    <div class="container">
+        <h1>Formulário de Doação de Livros</h1>
+        <form action="#" method="POST">
+            <label for="nome">Nome:</label>
+            <input type="text" id="nome" name="nome" required>
+
+            <label for="email">E-mail:</label>
+            <input type="email" id="email" name="email" required>
+
+            <label for="livro">Livro a ser doado:</label>
+            <input type="text" id="livro" name="livro" required>
+
+            <label for="livro">Autor do Livro:</label>
+            <input type="text" id="livro" name="autorLivro" required>
+
+            <label for="livro">Genero do Livro:</label>
+            <input type="text" id="livro" name="generoLivro" required>
+            
+
+            <input type="submit" value="Enviar">
+        </form>
+    </div>
+        
+ 
+    </main>
+
+    <!-- rodapé -->
+    <footer >
+    <div class="opening-hours">
+    <br>
+            <br>
+            <h3>OPENING HOURS</h3>
+            <ul>
+                <li><strong>Monday</strong> 6:00 – 23:00</li>
+                <br>
+            
+                <li><strong>Tuesday</strong> 6:00 – 23:00</li>
+                <br>
+           
+                <li><strong>Wednesday</strong> 6:00 – 23:00</li>
+                <br>
+            
+                <li><strong>Thursday</strong> 6:00 – 23:00</li>
+                <br>
+           
+                <li><strong>Friday</strong> 6:00 – 23:00</li>
+                <br>
+            
+                <li><strong>Saturday</strong> 3:00 – 23:00</li>
+                <br>
+          
+                <li><strong>Sunday</strong> 6:00 – 23:00</li>
+            </ul>
+        </div>
+        <div class="contacts">
+        <br>
+            <br>
+            <h3>CONTACTS</h3>
+            <br>
+            <p>@expresso_poesia</p>
+            <br>
+            <br>
+            
+            <p>expressopoesia@gmail.com</p>
+            <br>
+            <br>
+
+            <p>exepresso&poesia.com.br</p>
+            <br>
+            <br>
+            
+            <p>(+55) 77 98042-1803</p>
+            <br>
+            <br>
+            
+        </div>
+		
+</footer>
+
+  </body>
+</html>
